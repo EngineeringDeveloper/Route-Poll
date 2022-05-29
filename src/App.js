bde1b1cb707b55a953843abe9317be456626c82b
EngineeringDeveloper <ryan.c.parki@gmail.com>
2022-05-29 19:36:49 +0100

    Added Map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import './App.css';
-import {Map} from "react-map-gl"
+import "./App.css";
+import { Map } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useState } from "react";
 
-// Map with react map
+// Map with react map & Mapbox
 // https://visgl.github.io/react-map-gl/docs/get-started/get-started
 function App() {
-  return (
-    <div className="App">
-      <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-        }}
-        style={{ width: 600, height: 400 }}
-        mapStyle="mapbox:://styles/mapbox/streets-v9"/>
-    </div>
-  );
+    const [viewState, setViewState] = useState({
+      latitude: 54.9783,
+      longitude: -1.6178,
+        zoom: 10,
+    });
+
+    return (
+        <div className='App'>
+            <Map
+                {...viewState}
+                onMove={(evt) => setViewState(evt.viewState)}
+                style={{ width: 600, height: 400 }}
+                mapStyle='mapbox://styles/mapbox/streets-v9'
+            />
+        </div>
+    );
 }
 
 export default App;
