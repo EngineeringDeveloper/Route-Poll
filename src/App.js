1b1a406f98faad76ce9406785fcb6034a99ba0ee
EngineeringDeveloper <ryan.c.parki@gmail.com>
2022-05-30 21:16:48 +0100

    Render GPX route on DOM

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from "react";
 import "./App.css";
 import { Map, Source, Layer } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useState } from "react";
-// import { toGeoJSON } from "@mapbox/toGeoJSON"
-import { readFile } from 'fs'
-// import testGPX from ".//assets/Saturday Option .gpx"
-const togeojson = require("@tmcw/togeojson") // require("togeojson"); tmcw is maintained?
+import { useState, useEffect } from "react";
 
-const DomParser = require("xmldom").DOMParser; // node doesn't have xml parsing or a dom.
-
-// generate a geojson for a route
-const fileParsedFromDom = new DomParser().parseFromString(
-    // testGPX
-    readFile(".//assets/Saturday Option .gpx", { encoding: "UTF-8" })
-);
-
-// Convert GPX to GeoJSON
-console.log(fileParsedFromDom)
-const geojson = togeojson.gpx(fileParsedFromDom);
+// get the GPX files.
+import { gpx } from "@tmcw/togeojson";
+import testGPX from ".//assets/Saturday Option .gpx";
+import { DOMParser } from "@xmldom/xmldom";
+// const DomParser = require("@xmldom/xmldom").DomParser; // node doesn't have xml parsing or a dom.
 
 // const geojson = {
 //     type: 'FeatureCollection',
@@ -26,40 +16,59 @@ const geojson = togeojson.gpx(fileParsedFromDom);
 //       {type: 'Feature', geometry: {type: 'Point', coordinates: [-122.4, 37.8]}}
 //     ]
 //   };
-  
-const layerStyle = {
-    id: 'point',
-    type: 'circle',
-    paint: {
-        'circle-radius': 10,
-        'circle-color': '#007cbf'
-    }
-    };
 
+// https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#paint-property
+const layerStyle = {
+    id: "gpsLine",
+    type: "line",
+    paint: {
+        "line-color": "#fc4c02",
+        "line-width": 2.5,
+    },
+};
 
 // Map with react map & Mapbox
 // https://visgl.github.io/react-map-gl/docs/get-started/get-started
 function App() {
     const [viewState, setViewState] = useState({
-      latitude: 54.9783,
-      longitude: -1.6178,
+        latitude: 54.9783,
+        longitude: -1.6178,
         zoom: 10,
     });
 
+    const [geoInfo, setgeoInfo] = useState({
+        source: null,
+    });
+
+    useEffect(() => {
+        fetch(testGPX)
+            .then((r) => r.text())
+            .then((text) => {
+                // generate a geojson for a route
+                const gpxDom = new DOMParser().parseFromString(text, 'text/xml')
+                const geojson = gpx(gpxDom);
+                setgeoInfo({
+                    source: (
+                        <Source id='my-data' type='geojson' data={geojson}>
+                            <Layer {...layerStyle} />
+                        </Source>
+                        // <div>Test Finished</div>
+                    ),
+                });
+                // console.log(geojson);
+            });
+    }, [])
+
     return (
         <div className='App'>
-            {/* <div>{testGPX}</div> */}
-            <div>{fileParsedFromDom}</div>
-            {/* <Map
+            <Map
                 {...viewState}
                 onMove={(evt) => setViewState(evt.viewState)}
                 style={{ width: 600, height: 400 }}
                 mapStyle='mapbox://styles/mapbox/streets-v9'
-            />
-            <Source id="my-data" type="geojson" data={geojson}>
-            <Layer {...layerStyle} />
-            </Source> */}
-            <div>{geojson}</div>
+            >
+                {geoInfo.source}
+                </Map>
         </div>
     );
 }
